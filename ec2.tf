14796f5dbd16448df0092acfcb70cf9c3b96cca7
gr0undh0g <mattf0wl3r@gmail.com>
2019-05-29 18:59:12 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-2"
-  shared_credentials_file = "/home/matts/.aws/credentials"
+  shared_credentials_file = "/home/matts/.aws/creds"
   
   #profile                 = "customprofile"
 
